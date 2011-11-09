db1a6e05734cffc22061f7b083a77f18fb509c54
martinrusev <martinrusev@zoho.com>
2011-11-09 15:34:44 +0000

    Removed prints, addded json to the spec

--- lib/ramon.rb
+++ lib/ramon.rb
@@ -1,13 +1,16 @@
-require "lib/ramon/version"
-require "lib/ramon/logger"
-require "lib/ramon/remote"
-require "lib/ramon/exception_data"
+def require_local(file)
+  require File.join(File.dirname(__FILE__), 'ramon', file)
+end
+
+require_local "version"
+require_local "logger"
+require_local "remote"
+require_local "exception_data"
 
 module Ramon
 	def self.log(message, level=nil)
 		log_hash = Log.log(message, level)
 		Remote.post('log', log_hash)
 	end
-	log('boo me','info')
 end
 
--- lib/ramon/remote.rb
+++ lib/ramon/remote.rb
@@ -12,13 +12,12 @@ module Ramon
 			else
 				@url = '/api/exception'
 			end
-			puts data
 
 			req = Net::HTTP::Post.new(@url, initheader = {'Content-Type' =>'application/json'})
 			#req.body = Zlib::Deflate.deflate(data.to_json, Zlib::BEST_SPEED)
 			req.body = data.to_json
 			response = Net::HTTP.new(Config::host, Config::port).start {|http| http.request(req) }
-			puts "Response #{response.code} #{response.message}: #{response.body}"
+			#puts "Response #{response.code} #{response.message}: #{response.body}"
 		end
 	end # class end
 end # module end 
